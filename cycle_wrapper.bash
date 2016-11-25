235431c2c35338bd542febc81e6b89c8f3280495
nyirock <nyirock@gmail.com>
2016-11-24 20:30:14 -0700

    Start cyclic run if iterations gt 1

--- cycle_wrapper.bash
+++ cycle_wrapper.bash
@@ -2,7 +2,7 @@
 dir_name="cyclic"
 chunk_size=100
 increment=50
-iterations=20
+iterations=1
 rm -rf $dir_name
 mkdir $dir_name
 location=$dir_name"/it_1"
@@ -11,11 +11,17 @@ alen="100"
 #echo $location
 #python mg_blast_wrapper_v1.8_cyclic.py     -r all_AP_WPS-2_bacterium.fna -m pp_metagenome3_assembled.fasta -n $location -shear $chunk_size
 
-for (( c=0; c<=iterations; c++ ))
-do
-   location=$dir_name"/it_"$c
-   #echo $location
-   chunk_size=$(($chunk_size+$increment*$c))
-   #echo $chunk_size
-   python mg_blast_wrapper_v1.10.py    -r all_AP_WPS-2_bacterium.fna -m pp_metagenome3_assembled.fasta -n $location --shear $chunk_size -f $format -a $alen
-done
+if [ "$iterations" -gt "1" ]
+then
+    for (( c=0; c<=iterations; c++ ))
+    do
+       location=$dir_name"/it_"$c
+       #echo $location
+       chunk_size=$(($chunk_size+$increment*$c))
+       #echo $chunk_size
+       python mg_blast_wrapper_v1.10.py    -r all_AP_WPS-2_bacterium.fna -m pp_metagenome3_assembled.fasta -n $location --shear $chunk_size -f $format -a $alen
+    done
+else
+    location=$dir_name
+    python mg_blast_wrapper_v1.10.py    -r all_AP_WPS-2_bacterium.fna -m pp_metagenome3_assembled.fasta -n $location --shear $chunk_size -f $format -a $alen
+fi
